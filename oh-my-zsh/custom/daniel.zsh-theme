211793115f4197e344dbfe9e80728be07e58e8b1
Daniel Thor <daniel.thor@gmail.com>
2012-12-27 16:59:00 +0100

    extended git prompt with base dir and sub dir

--- oh-my-zsh/custom/daniel.zsh-theme
+++ oh-my-zsh/custom/daniel.zsh-theme
@@ -1,4 +1,6 @@
-PROMPT='%{$fg[blue]%}%3~%{$reset_color%}$(git_prompt_info)%# '
+PROMPT='%{$fg[blue]%}$(git_prompt_base_dir)%{$reset_color%}$(git_prompt_info)%{$fg[green]%}$(git_prompt_sub_dir)%{$reset_color%}%# '
 
 ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg[green]%}[%{$reset_color%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$fg[green]%}]%{$reset_color%}"
+ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg_no_bold[red]%}*%{$reset_color%}"
+ZSH_THEME_GIT_PROMPT_EXTENDED_PWD="%3~"
--- oh-my-zsh/custom/plugins/daniel/daniel.plugin.zsh
+++ oh-my-zsh/custom/plugins/daniel/daniel.plugin.zsh
@@ -6,6 +6,26 @@ h() { cd ~/$1; }
 _h() { _files -W ~/ -/; }
 compdef _h h
 
+function git_prompt_base_dir {
+  git_dir=`git rev-parse --show-toplevel 2> /dev/null`
+  if [ -n "$git_dir" ]; then
+    # in a git dir
+    echo `basename $git_dir`
+  else
+    # not in a git dir
+    echo $ZSH_THEME_GIT_PROMPT_EXTENDED_PWD
+  fi
+}
+
+function git_prompt_sub_dir {
+  sub_dir=`git rev-parse --show-prefix 2> /dev/null`
+  if [ -n "$sub_dir" ]; then
+    echo "/${sub_dir%/}"
+  else
+    echo ""
+  fi
+}
+
 export EDITOR='vim'
 
 # autocorrect is more annoying than helpful
